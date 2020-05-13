b62e9c630e38d01358524960e5413a093e0a57f8
IsmaelCleiton <ismaelcleitonsf@gmail.com>
2020-05-13 17:37:27 -0300

    FULL UPDATE

--- js/js.js
+++ js/js.js
@@ -1,1 +1,11 @@
-alert("!!!!!!!!!!!!!!!!!!!!!!!!")
+var X = 0;
+
+function Message( message ){
+    alert(message);
+}
+
+function Easter(X){
+    if(X == 10){
+        alert('parabens!');
+    }
+}
